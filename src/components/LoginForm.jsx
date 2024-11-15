8891649301ffdc5b99442897bb58ec4ffb09e4be
Vbroendum <32537066+Vbroendum@users.noreply.github.com>
2024-11-15 11:09:12 +0100

    forside design + knap med routing

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,30 @@
+import { PasswordInput, Input, Button, Checkbox, } from '@mantine/core';
+import { useRouter } from '@tanstack/react-router';
+
+export default LoginForm
+
+function LoginForm() {
+    const router = useRouter();
+
+  return (
+    <div style={{width: "100%", margin: "24px", alignContent: "center"}}>
+        <h1>EFIF</h1>
+        <h3>Log på</h3>
+      
+      <div>
+        <Input.Wrapper style={{marginBottom: "24px" }} required label="Brugernavn">
+        <Input placeholder="Indtast brugernavn" />
+        </Input.Wrapper>
+        <PasswordInput style={{marginTop: "34px" }} required
+        label="Password"
+        placeholder="password"
+    />
+      </div>
+      <Checkbox style={{ marginTop: "24px" }}
+      defaultChecked
+      label="Husk mig"
+    />
+    <Button style={{ marginTop: "24px" }} variant="filled" onClick={() => router.navigate({ to: "/about" })}>Log ind</Button>
+    </div>
+  );
+}
--- src/routes/index.lazy.jsx
+++ src/routes/index.lazy.jsx
@@ -1,4 +1,5 @@
 import { createLazyFileRoute, useRouteContext } from "@tanstack/react-router";
+import LoginForm from "../components/LoginForm";
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
@@ -8,9 +9,18 @@ function Index() {
   const context = useRouteContext({ from: "/" });
   console.log(context);
 
+  const imageStyle = {
+    width: "100%",
+    height: "100vh",
+    objectFit: "cover",
+  };
+
+
+
   return (
-    <div>
-      <h3>Welcome Home!</h3>
-    </div>
+    <div style={{ display: "flex"}}>
+      <img src="src/assets/image-2.webp" alt="" style={imageStyle} />
+      <LoginForm />
+      </div>
   );
 }
