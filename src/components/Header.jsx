f4bb122c98829a060c395b273db6814c83a57231
Buchandreas <43332738+Buchandreas@users.noreply.github.com>
2024-11-20 12:16:41 +0100

    Updated links in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from '../assets/CPHBusiness_Logo.svg';
+import { Link } from '@tanstack/react-router';
 
 function Header() {
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between', padding: '15px', backgroundColor: '#228BE6', color: 'white', fontWeight: '500' }}>
-      <img src={logo} alt="CPH Business Logo" width="100" />
+        <Link to="/frontpage"><img src={logo} alt="CPH Business Logo" width="100" /></Link>
       <div style={{ display: 'flex', gap: '20px' }}>
-        <div>Forside</div>
-        <div>Mine Bookinger</div>
+        <div><Link to="/">Forside</Link></div>
+        <div><Link to="/" style={{textDecoration: 'none'}}>Mine bookinger</Link></div>
       </div>
     </div>
   );
