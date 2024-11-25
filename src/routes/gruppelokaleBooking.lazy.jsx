4632539ef26e3d25a7fcf0f6fdf5218ebadfd7c9
Vbroendum <32537066+Vbroendum@users.noreply.github.com>
2024-11-25 16:12:28 +0100

    ændringer på diverse sider

--- src/routes/gruppelokaleBooking.lazy.jsx
+++ src/routes/gruppelokaleBooking.lazy.jsx
@@ -1,5 +1,5 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
-import Header from '../components/header'
+import Header from '../components/Header'
 
 export const Route = createLazyFileRoute('/gruppelokaleBooking')({
   component: RouteComponent,
