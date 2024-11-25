60da5341d272f795ad5f4a6805f2af7714fd4d31
Vbroendum <32537066+Vbroendum@users.noreply.github.com>
2024-11-25 12:29:51 +0100

    stepper component in the works

--- src/routes/startBooking.lazy.jsx
+++ src/routes/startBooking.lazy.jsx
@@ -1,9 +1,11 @@
 import { createLazyFileRoute, useRouteContext } from '@tanstack/react-router'
+import { useState } from 'react';
 
 import CustomCalendar from '../components/CustomCalendar';
 import LokaleForm from '../components/Lokaleform';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import StepperComponent from '../components/Steps';
 
 
 export const Route = createLazyFileRoute('/startBooking')({
@@ -13,6 +15,8 @@ export const Route = createLazyFileRoute('/startBooking')({
 function StartBooking() {
   const context = useRouteContext({ from: "/startBooking" });
   console.log(context);
+
+  const [activeStep, setActiveStep] = useState(0);
 
   const bookingStyle = {
     display: "flex",
@@ -24,10 +28,25 @@ function StartBooking() {
   }
 
 
+  const steps = [
+    {
+      label: "Step 1",
+    },
+    {
+      label: "Step 2",
+    },
+    {
+      label: "Step 3",
+    },
+    {
+      label: "Step 4",
+    },
+  ];
+
   return (
     <div>
       <Header />
-
+      <StepperComponent steps={steps} activeStep={activeStep} setActiveStep={setActiveStep} style={{ margin: "24px" }}/>
     <div style={bookingStyle}>
 
     <CustomCalendar style={{ marginLeft: "24px" }} />
